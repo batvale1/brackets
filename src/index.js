d34259e7d68ece5c4fd364929ada3ccd72a3d508
batvale1 <batvale1@yandex.ru>
2020-10-12 16:50:58 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const strAsString = str.split('');
+
+    while (true) {
+        let foundSequence = false;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            const [ leftBracket, rightBracket] = bracketsConfig[i];
+
+            for (let i = 0; i < strAsString.length; i++) {
+                if (strAsString[i] === leftBracket && strAsString[i + 1] === rightBracket) {
+                    strAsString.splice(i + 1, 1);
+                    strAsString.splice(i, 1);
+                    foundSequence = true;
+                    break;
+                }
+            }
+
+        }
+
+        if (!foundSequence) {
+            break;
+        }
+    }
+
+    return strAsString.length <= 0;
 }
